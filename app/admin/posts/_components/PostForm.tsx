63cee90d912362c9b014e9cd9c514123881b7897
shuheitachikawa <shuhei.tusx@gmail.com>
2026-01-05 14:51:43 +0900

    s

--- app/admin/posts/_components/PostForm.tsx
+++ app/admin/posts/_components/PostForm.tsx
@@ -1,7 +1,10 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { CategoriesSelect } from './CategoriesSelect'
 import { Category } from '@/app/api/admin/posts/[id]/route'
+import { v4 as uuidv4 } from 'uuid'  // 固有IDを生成するライブラリ
+import { supabase } from '@/app/_libs/supabase'
+
 
 interface Props {
   mode: 'new' | 'edit'
@@ -32,6 +35,40 @@ export const PostForm: React.FC<Props> = ({
   onDelete,
   disabled
 }) => {
+  const [thumbnailImageKey, setThumbnailImageKey] = useState('')
+
+
+  const handleImageChange = async (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): Promise<void> => {
+    if (!event.target.files || event.target.files.length == 0) {
+      // 画像が選択されていないのでreturn
+      return
+    }
+
+    const file = event.target.files[0] // 選択された画像を取得
+
+    const filePath = `private/${uuidv4()}` // ファイルパスを指定
+
+    // Supabaseに画像をアップロード
+    const { data, error } = await supabase.storage
+      .from('post_thumbnail')　// ここでバケット名を指定
+      .upload(filePath, file, {
+        cacheControl: '3600',
+        upsert: false,
+      })
+
+    // アップロードに失敗したらエラーを表示して終了
+    if (error) {
+      alert(error.message)
+      return
+    }
+
+    // data.pathに、画像固有のkeyが入っているので、thumbnailImageKeyに格納する
+    setThumbnailImageKey(data.path)
+  }
+
+
   return (
     <form onSubmit={onSubmit} className="space-y-4">
       <div>
@@ -67,19 +104,12 @@ export const PostForm: React.FC<Props> = ({
       </div>
       <div>
         <label
-          htmlFor="thumbnailUrl"
+        htmlFor="thumbnailImageKey"
           className="block text-sm font-medium text-gray-700"
         >
           サムネイルURL
         </label>
-        <input
-          type="text"
-          id="thumbnailUrl"
-          value={thumbnailUrl}
-          onChange={(e) => setThumbnailUrl(e.target.value)}
-          className="mt-1 block w-full rounded-md border border-gray-200 p-3"
-          disabled={disabled}
-        />
+        <input type="file" id="thumbnailImageKey" onChange={handleImageChange} accept="image/*" />
       </div>
       <div>
         <label
